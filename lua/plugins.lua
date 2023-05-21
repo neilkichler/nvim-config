aba60151244f4c01d1ce2f0be85690d026abc878
Neil Kichler <neil.kichler@gmail.com>
2023-05-21 21:00:15 +0200

    ensure packer is installed

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,3 +1,16 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
 return require('packer').startup(function(use)
     -- package manager
     use 'wbthomason/packer.nvim'
@@ -79,7 +92,7 @@ return require('packer').startup(function(use)
         config = function() require('trouble').setup {} end
     }
 
-    use 'Exafunction/codeium.vim'
+    -- use 'Exafunction/codeium.vim'
     -- use 'neanias/everforest-nvim'
     -- require('everforest').setup({ background = "hard" })
     -- require('everforest').load()
