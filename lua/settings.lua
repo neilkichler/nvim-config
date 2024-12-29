5989a5bba5245a57ae2a5efefdb786a6ad1398cd
Neil Kichler <neil.kichler@gmail.com>
2024-12-29 15:00:13 +0100

    add maplocalleader=' '

--- lua/settings.lua
+++ lua/settings.lua
@@ -1,33 +1,34 @@
 -- Space as leader key.
-vim.g.mapleader     = ' '
+vim.g.mapleader      = ' '
+vim.g.maplocalleader = ' '
 
 -- Remove the highlight after searching for text.
-vim.o.hlsearch      = false
+vim.o.hlsearch       = false
 
 -- Enable break indent.
-vim.o.breakindent   = true
+vim.o.breakindent    = true
 
 -- Case insensitive searching unless /C or capital in search.
-vim.o.ignorecase    = true
-vim.o.smartcase     = true
+vim.o.ignorecase     = true
+vim.o.smartcase      = true
 
 -- Decrease update time.
-vim.o.updatetime    = 250
+vim.o.updatetime     = 250
 
 -- Set colorscheme defaults.
-vim.o.termguicolors = true
+vim.o.termguicolors  = true
 
 -- Save undo history.
-vim.o.undofile      = true
+vim.o.undofile       = true
 
 -- Tab settings.
-vim.o.tabstop       = 4
-vim.o.shiftwidth    = 4
-vim.o.softtabstop   = 4
-vim.o.expandtab     = true
-vim.o.autoread      = true
-vim.o.splitbelow    = true
-vim.o.splitright    = true
+vim.o.tabstop        = 4
+vim.o.shiftwidth     = 4
+vim.o.softtabstop    = 4
+vim.o.expandtab      = true
+vim.o.autoread       = true
+vim.o.splitbelow     = true
+vim.o.splitright     = true
 
 -- It takes forever on WSL2 to load the clipboard.
 -- Define it upfront so neovim doesn't need to search for it.
