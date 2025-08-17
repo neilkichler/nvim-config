a894bf6e74ca0c688c21886474152cd2575b9a88
Neil Kichler <neil.kichler@gmail.com>
2025-08-17 17:29:06 +0200

    remove debugger keymaps for now (I don't use it)

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -78,27 +78,3 @@ vim.api.nvim_create_autocmd("LspAttach", {
         nmap('<leader>f', vim.lsp.buf.format, 'Format')
     end,
 })
-
--- Debugger set breakpoint
-keymap("n", "<leader>db", "<cmd>DapToggleBreakpoint<CR>", { desc = "Add breakpoint at line" })
-
--- Debugger step into
-keymap("n", "<leader>di", "<cmd>DapStepInto<CR>", { desc = "Step into" })
-
--- Debugger step over
-keymap("n", "<leader>do", "<cmd>DapStepOver<CR>", { desc = "Step over" })
-
--- Debugger step out
-keymap("n", "<leader>ds", "<cmd>DapStepOut<CR>", { desc = "Step out" })
-
--- Debugger continue
-keymap("n", "<leader>dc", "<cmd>DapContinue<CR>", { desc = "Continue" })
-
--- Debugger restart
-keymap("n", "<leader>dr", "<cmd>DapRestart<CR>", { desc = "Restart" })
-
--- Debugger terminate
-keymap("n", "<leader>dt", "<cmd>DapTerminate<CR>", { desc = "Terminate" })
-
--- Debugger toggle UI
-keymap("n", "<leader>du", "<cmd>DapUIToggle<CR>", { desc = "Toggle UI" })
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -247,54 +247,6 @@ local plugins = {
             -- VimTeX configuration goes here
         end
     },
-    -- -- Debugger
-    -- {
-    --     "mfussenegger/nvim-dap",
-    --     event = "VeryLazy",
-    --     dependencies = { "nvim-neotest/nvim-nio" },
-    -- },
-    -- {
-    --     "mfussenegger/nvim-dap-python",
-    --     event = "VeryLazy",
-    -- },
-    -- {
-    --     "theHamsta/nvim-dap-virtual-text",
-    --     event = "VeryLazy",
-    -- },
-    -- {
-    --     "rcarriga/nvim-dap-ui",
-    --     event = "VeryLazy",
-    --     dependencies = { "mfussenegger/nvim-dap" },
-    --     config = function()
-    --         local dap = require("dap")
-    --         local dapui = require("dapui")
-    --         dapui.setup()
-    --         dap.listeners.after.event_initialized["dapui_config"] = function()
-    --             dapui.open()
-    --         end
-    --         dap.listeners.before.event_terminated["dapui_config"] = function()
-    --             dapui.close()
-    --         end
-    --         dap.listeners.before.event_exited["dapui_config"] = function()
-    --             dapui.close()
-    --         end
-    --     end
-    -- },
-    -- {
-    --     "jay-babu/mason-nvim-dap.nvim",
-    --     event = "VeryLazy",
-    --     dependencies = { "williamboman/mason.nvim", "mfussenegger/nvim-dap" },
-    --     opts = {
-    --         handlers = {}
-    --     },
-    --     config = function()
-    --         require("mason-nvim-dap").setup({
-    --             ensure_installed = { "python" },
-    --
-    --             automatic_installation = true,
-    --         })
-    --     end
-    -- },
 }
 
 require('lazy').setup(plugins, {
